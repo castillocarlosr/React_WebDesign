7ccf32df002a0ec538570984b63d0caf70ae7633
CARLOS R. CASTILLO <castillocarlosr@gmail.com>
2023-05-03 16:18:06 -0700

    added about me page starter

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import About from './components/About';
 
 function App(props) {
   return (
@@ -12,26 +13,29 @@ function App(props) {
         <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Carlos is re-learning react since it's been a few years since I had to use it.
         </a> */}
-      <div class="top">
-        {/* LOGO */}
-        <div id="logo">
-					{/* <span class="image avatar48"><img src="images/carlosProfile2.JPG" alt="A logo picture of Carlos Castillo" /></span> */}
-					<h1 id="title">Carlos R. Castillo</h1>
-					<p>Software Developer</p>
-				</div>
-        <nav id="nav">
-          <ul>
-					  <li><a href="#top" id="top-link"><span class="icon fa-home">Intro</span></a></li>
-						<li><a href="#skills" id="portfolio-link"><span class="icon fa-code">Skills</span></a></li>
-						<li><a href="#portfolio" id="portfolio-link"><span class="icon fa-th">Portfolio</span></a></li>
-						<li><a href="#about" id="about-link"><span class="icon fa-user-circle">About Me</span></a></li>
-						<li><a href="#resume" id="about-resume"><span class="icon fa-file-word-o">Resume</span></a></li>
-						<li><a href="#github" id="github-link"><span class="icon fa-github-square">GitHub</span></a></li>
-						<li><a href="#contact" id="contact-link"><span class="icon fa-envelope">Contact</span></a></li>
-					</ul>
-        </nav>
-      </div>
+        <div class="top">
+          {/* LOGO */}
+          <div id="logo">
+            {/* <span class="image avatar48"><img src="images/carlosProfile2.JPG" alt="A logo picture of Carlos Castillo" /></span> */}
+            <h1 id="title">Carlos R. Castillo</h1>
+            <p>Software Developer</p>
+          </div>
+          <nav id="nav">
+            <ul>
+              <li><a href="#top" id="top-link"><span class="icon fa-home">Intro</span></a></li>
+              <li><a href="#skills" id="portfolio-link"><span class="icon fa-code">Skills</span></a></li>
+              <li><a href="#portfolio" id="portfolio-link"><span class="icon fa-th">Portfolio</span></a></li>
+              <li><a href="#about" id="about-link"><span class="icon fa-user-circle">About Me</span></a></li>
+              <li><a href="#resume" id="about-resume"><span class="icon fa-file-word-o">Resume</span></a></li>
+              <li><a href="#github" id="github-link"><span class="icon fa-github-square">GitHub</span></a></li>
+              <li><a href="#contact" id="contact-link"><span class="icon fa-envelope">Contact</span></a></li>
+            </ul>
+          </nav>
+        </div>
       </header>
+      <main>
+        <About />
+      </main>
     </div>
   );
 }
---
+++ my-app/src/components/About.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function About(){
+    return(
+        <section id="about">
+            <h1>Hi, it's me.  I'm the problem.</h1>
+        </section>
+    );
+}
