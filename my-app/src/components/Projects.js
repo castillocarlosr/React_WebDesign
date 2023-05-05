afaeb3434d00c00a623908ca0141391950a00923
Carlos Roberto Castillo <39848540+castillocarlosr@users.noreply.github.com>
2023-05-04 23:06:54 -0700

    added test project

---
+++ my-app/src/components/Projects.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Projects(){
+    return(
+        <section id="projects">
+            <h1>Hi, it's me.  I'm the problem.</h1>
+            <h6>Projects go here</h6>
+        </section>
+    );
+}
